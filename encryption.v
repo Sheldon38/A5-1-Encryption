a419aaf4e2e123b9818337f10bbc0a4222a3cc76
Sheldon38 <109095852+Sheldon38@users.noreply.github.com>
2022-07-19 12:50:24 +0530

    Update encryption.v

--- encryption.v
+++ encryption.v
@@ -4,7 +4,7 @@ input [7:0] inbyte,
 input hkey,
 output reg [7:0] outbyte,
 output reg ready=0,
-output reg key
+output reg read=0
 );
 
 reg [18:0] reg1=0;
@@ -53,6 +53,7 @@ begin
         begin
             @(posedge clk)
             begin
+            read=1'b1;
             maximum_ABC(max,reg1[10],reg2[11],reg3[12]);
             xor1=reg1[5]^reg1[2]^reg1[1]^reg1[0];
             xor2=reg2[1]^reg2[0];
@@ -75,37 +76,48 @@ begin
                 reg3[22]=xor3;
             end
             encryptedkey=reg1[0]^reg2[0]^reg3[0];
-            key=encryptedkey;
             
-            $display("%b",encryptedkey);
-            // temporary_outbyte[bitshift]=encryptedkey^inbyte[bitshift];
-            // bitshift=bitshift+1;
-            // if(bitshift==8)
-            // begin
-                // bitshift=0;
-                // outbyte=temporary_outbyte;
-                // ready=1'b1;
-            // end
-            // else
-            // ready=1'b0;
+            // $display("%b",encryptedkey);
+            temporary_outbyte[bitshift]=encryptedkey^inbyte[bitshift];
+            bitshift=bitshift+1;
+            if(bitshift==8)
+            begin
+                bitshift=0;
+                outbyte=temporary_outbyte;
+                ready=1'b1;
+            end
+            else
+            ready=1'b0;
             end
         end
         //end loop here
 end
 endmodule
 
+//--------------------------------------------------Test bench Module--------------------------------------------------------------------------------------------//
+
 module tbencryption;
 reg clk,hkey;
-wire key;
+reg [7:0] inbyte;
+wire [7:0] outbyte;
+wire read,ready;
 
 encryption e1(
 .clk (clk),
 .hkey (hkey),
-.key (key)
+.read (read),
+.ready (ready),
+.inbyte (inbyte),
+.outbyte (outbyte)
 );
 
 reg [63:0] hardware="hardware";
+reg [7:0] IMAGE [0:65535];
+reg [7:0] IMAGEOUT [0:65535];
 integer count1=0;
+
+initial
+    $readmemh("E:/clg work/Sem 4/Project 2/imtotxt.txt",IMAGE);
 
 initial
 begin
@@ -124,6 +136,20 @@ begin
         hkey=hardware[count1];
         end
     end
- 
+    
+    count1=0;
+    if(read)
+    begin
+        while(count1<=65535)
+        begin
+        inbyte=IMAGE[count1];
+            if(ready)
+            begin
+            IMAGEOUT[count1]=outbyte;
+            count1=count1+1;
+            end
+        end
+    end
+
 end
 endmodule
